0cc2d9149ee6820694e5a873a545547c88003d30
Akash Sawan <akash27274@gmail.com>
2023-07-30 14:27:44 +0530

    Completed first CodeClauseProject

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Translate from "./components/Translate";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Translate />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
